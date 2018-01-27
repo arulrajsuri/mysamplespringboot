8624173d5311c1f3da9db1dbf8fa687ad6cbd106
T1668A0 <T1668A0@CITMN5CG7221XYG.cag.cdroot.net>
2018-01-27 16:07:45 -0500

    commit

---
+++ src/main/java/com/sample/maven/samplemaven/samplecontroller.java
@@ -0,0 +1,14 @@
+package com.sample.maven.samplemaven;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class samplecontroller {
+
+	@RequestMapping("/getvalue")
+	public String getvalue()
+	{
+		return "Jenkins";
+	}
+}
