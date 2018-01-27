33a72b692adea42ccc04faa941a132997cb64fd3
T1668A0 <T1668A0@CITMN5CG7221XYG.cag.cdroot.net>
2018-01-27 15:56:42 -0500

    commit

---
+++ src/main/java/com/sample/maven/samplemaven/SamplemavenApplication.java
@@ -0,0 +1,13 @@
+package com.sample.maven.samplemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplemavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplemavenApplication.class, args);
+		System.out.println("sdfsdknhlsgk");
+	}
+}
